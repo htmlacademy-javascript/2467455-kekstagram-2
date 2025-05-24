6e45c95dbe411504a3794fb02e580db6df1b4370
Keks <keks@htmlacademy.ru>
2025-05-24 16:59:55 +0300

    :heavy_check_mark: Сборка #4

---
+++ 4/js/main.js
@@ -0,0 +1,6 @@
+import './util.js';
+import {photoDescriptions} from './data.js';
+
+console.log(
+  photoDescriptions
+);
---
+++ 4/js/util.js
@@ -0,0 +1,7 @@
+// Вспомогательные функции
+const getRandomInteger = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+
+const getRandomArrayElement = (array) => array[getRandomInteger(0, array.length - 1)];
+
+export {getRandomArrayElement};
+export {getRandomInteger};
