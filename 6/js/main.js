bfc66c02746d81271f6c33d0a2454ca538f12b5d
Keks <keks@htmlacademy.ru>
2025-06-09 18:33:26 +0300

    :heavy_check_mark: Сборка #6

---
+++ 6/js/main.js
@@ -0,0 +1,8 @@
+import './util.js';
+import './template.js';
+import {photoDescriptions} from './data.js';
+import {renderThumbnails} from './template.js';
+console.log(
+  photoDescriptions
+);
+console.log (renderThumbnails);
