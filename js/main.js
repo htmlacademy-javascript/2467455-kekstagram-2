ed159ab9710b6559e5ac287df43def88345f7894
MadinaSatem <151987064+MadinaSatem@users.noreply.github.com>
2025-06-11 14:22:12 +0300

    Отправляет на вторуб проверку

--- js/main.js
+++ js/main.js
@@ -1,9 +1,7 @@
 import './util.js';
-import './render.js';
 import {getPhotoDescriptions} from './data.js';
 import {renderThumbnails} from './render.js';
 
 const photoDescriptions = getPhotoDescriptions();
 renderThumbnails(photoDescriptions);
 
-console.log(getPhotoDescriptions());
