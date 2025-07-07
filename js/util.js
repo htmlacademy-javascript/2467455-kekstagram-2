71df3b2df92d47b4a62848a8935c6dbd127a546d
MadinaSatem <151987064+MadinaSatem@users.noreply.github.com>
2025-07-07 18:36:37 +0300

    Перламутровые пуговицы

--- js/util.js
+++ js/util.js
@@ -7,5 +7,14 @@ const isEscapeKey = (evt) => evt.key === 'Escape';
 
 const isEnterKey = (evt) => evt.key === 'Enter';
 
+function debounce(callback, delay = 500) {
+  let timeoutId;
+  return (...args) => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => callback(...args), delay);
+  };
+}
+
 export {getRandomArrayElement, isEscapeKey, isEnterKey};
 export {getRandomInteger};
+export { debounce };
